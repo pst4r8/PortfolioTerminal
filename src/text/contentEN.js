35662cdb98448b4631a171fe2a0b658773976179
Pst4r8 <pst4r8@gmail.com>
2023-01-09 12:57:50 +0700

    portfolio

---
+++ src/text/contentEN.js
@@ -0,0 +1,53 @@
+var contentEN = {
+  "aboutme" : {
+    "name" : "About me",
+    "content" : 
+<>
+<p>
+Hi 👋, I'm <a rel="noreferrer" target="_blank" href="https://www.github.com/pst4r8">@pst4r8</a>,<br/>
+I'm a computer science student for at least one year and a programmer self-student for at least two. I like to program, I like to think about how to design software, structure projects, and how to write elegant code.
+</p>
+<p>
+Moreover, I like to study programmer languages, functional programming, clean code, and talk about Linux, open-source, and tech in general. 
+</p>
+<p>
+I have developed personal projects in which I work with web apps, APIs, bots, and databases. I used concepts of functional programming, object-oriented programming, REST API, and image processing.
+</p>
+<p>
+See the section about my programmer skills to see my projects and languages that I used, or contact me if you have an interest.
+</p>
+</>
+  },
+  "skills" : {
+    "name" : "Skills", 
+    "content" : 
+<>
+<p>
+To work with image processing, bots, and automation I used Python. Here's one bot that I made, and a script where I use image processing to create a fractal.
+</p>
+<p>
+To develop web apps and APIs I used JavaScript, ReactJs, and ExpressJS. Here's a library that I developed with this language.
+</p>
+<p>
+Besides, I know and like many concepts of functional programming, where I learned from the Haskell language and javascript. See a package where I used the beauty of the Haskell type system.
+</p>
+<p>
+See my <a rel="noreferrer" target="_blank" href="https://github.com/pst4r8">github account</a> to known more about these projects.
+</p>
+</>
+  },
+  "contact" : {
+    "name" : "Contact",
+    "content" : 
+<>
+<p>I'm open to work on private or open-source projects, or only talk with you, so feels free to send me a message, I will like to talk.</p>
+<p>Here my contact info :</p>
+<p> 💻 <a rel="noreferrer" target="_blank" href="https://github.com/pst4r8">My Github</a></p>
+<p> 👔 <a rel="noreferrer" target="_blank" href="https://www.linkedin.com/in/pst4r8/">My Linkedin</a></p>
+<p> 📞 <a rel="noreferrer" target="_blank" href="https://t.me/pst4r8">My Telegram</a></p>
+<p> 📨 <a href="mailto:pst4r8@gmail.com">pst4r8@gmail.com</a></p>
+</>
+  }
+}
+
+export default contentEN
---
+++ src/text/contentPT.js
@@ -0,0 +1,53 @@
+var contentPT = {
+  "aboutme" : {
+    "name" : "Sobre mim",
+    "content" :
+<>
+<p>
+Oi 👋, Eu sou <a rel="noreferrer" target="_blank" href="https://www.github.com/pst4r8">@pst4r8</a>,<br/>
+Sou estudante de informática há pelo menos um ano e programador autodidata há pelo menos dois. Gosto de programar, gosto de pensar em como projetar software, estruturar projetos e como escrever códigos elegantes.
+</p>
+<p>
+Além disso, gosto de estudar linguagens de programação, programação funcional, código limpo e falar sobre Linux, código aberto e tecnologia em geral.
+</p>
+<p>
+Desenvolvi projetos pessoais nos quais trabalho com web apps, APIs, bots e bancos de dados. Usei conceitos de programação funcional, programação orientada a objetos, API REST e processamento de imagens.
+</p>
+<p>
+Veja a seção sobre minhas habilidades de programador para ver meus projetos e linguagens que usei, ou entre em contato se tiver interesse.
+</p>
+</>
+  },
+  "skills" : {
+    "name" : "Habilidades",
+    "content" :
+<>
+<p>
+Para trabalhar com processamento de imagens, bots e automação eu usei Python. Aqui está um bot que fiz e um script onde uso processamento de imagem para criar um fractal.
+</p>
+<p>
+Para desenvolver aplicativos web e APIs, usei JavaScript, ReactJs e ExpressJS. Aqui está uma biblioteca que desenvolvi com esta linguagem.
+</p>
+<p>
+Além disso, conheço e gosto de muitos conceitos de programação funcional, onde aprendi da linguagem Haskell e javascript. Veja um pacote onde usei a beleza do sistema de tipo Haskell.
+</p>
+<p>
+Veja minha conta no <a rel="noreferrer" target="_blank" href="https://github.com/pst4r8">Github</a> para conhecer mais sobre esses projetos.
+</p>
+</>
+  },
+  "contact" : {
+    "name" : "Contato",
+    "content" : 
+<>
+<p>Estou aberto para trabalhar em projetos privados ou de código aberto, ou apenas conversar com você, então fique a vontade para me enviar uma mensagem, terei prazer em conversar.</p>
+<p>Aqui minhas informações de contato :</p>
+<p> 💻 <a rel="noreferrer" target="_blank" href="https://github.com/pst4r8">Meu Github</a></p>
+<p> 👔 <a rel="noreferrer" target="_blank" href="https://www.linkedin.com/in/pst4r8/">Meu Linkedin</a></p>
+<p> 📞 <a rel="noreferrer" target="_blank" href="https://t.me/pst4r8">Meu Telegram</a></p>
+<p> 📨 <a href="mailto:raulrufino0@gmail.com">raulrufino0@gmail.com</a></p>
+</>
+  }
+}
+
+export default contentPT
